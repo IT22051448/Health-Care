2e27674ea44ed7c69e8e5beb8fd5431f29f26714
Manilka Navod <manilka.projects@gmail.com>
2024-10-20 10:48:15 +0530

    signup - other gender

--- frontend/src/pages/customer/profile/profile.jsx
+++ frontend/src/pages/customer/profile/profile.jsx
@@ -1,5 +1,38 @@
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useSelector } from "react-redux";
+
 const ShoppingProfile = () => {
-  return <div>Home</div>;
+  const { user } = useSelector((state) => state.auth);
+
+  console.log(user);
+  return (
+    <div className="p-4">
+      <Card className="p-6 shadow-lg">
+        <h2
+          className="text-lg font-semibold text-center
+"
+        >
+          Your QR Code
+        </h2>
+        {user ? (
+          <div className="my-4">
+            <img src={user.QRCodeUrl} alt="QR Code" className="w-64 h-64" />
+          </div>
+        ) : (
+          <p>No QR code available.</p>
+        )}
+        <Button
+          onClick={() => {
+            console.log("Download QR");
+          }}
+          disabled={!user.qrCodeUrl}
+        >
+          Download QR Code
+        </Button>
+      </Card>
+    </div>
+  );
 };
 
 export default ShoppingProfile;
